fbb45e1da529345d3aa53165427dcfcfeeef66b7
Tiago Lima <tiaguinho.alima@gmail.com>
2020-08-13 15:45:34 -0300

    primeiro comando DML

---
+++ aula_05-inserindo_dados.sql
@@ -0,0 +1,25 @@
+insert into 
+	pessoas (nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+	('Godofredo', '1999-01-02', 'M', '78.5', '1.83', default);
+insert into 
+	pessoas (nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+	('Maria', '1999-03-12', 'F', '55.0', '1.60', 'Portugal');
+insert into 
+	pessoas (nome, nascimento, sexo, peso, altura, nacionalidade)
+values
+	('Creuza', '1943-12-12', 'F', '50.0', '1.53', 'Brasil');
+insert into 
+	pessoas
+values
+	(default, 'Mirjan', '1986-05-01', 'F', '70.4', '1.80', 'Alemanha');
+    
+insert into 
+	pessoas
+values
+	(default, 'Guillermo', '1998-09-30', 'M', '80.4', '1.87', 'Argentina'),
+    (default, 'Mario', '1976-04-11', 'M', '66.4', '1.70', 'Itália'),
+    (default, 'Megan', '1966-08-01', 'F', '60.4', '1.68', 'Canadá'),
+    (default, 'Anya', '1956-02-03', 'F', '70.4', '1.80', 'Rússia');
+
